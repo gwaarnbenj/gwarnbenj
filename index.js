6c9930533e20a4a4047ad7a2d1a98cb15b55fdb4
Ben Rheeston <benj@Bens-MacBook-Pro.local>
2021-08-06 19:18:24 +0100

    new files for tic tac toe

---
+++ index.js
@@ -0,0 +1,30 @@
+let isApproved = false;
+
+let person = {
+    name: 'Benj',
+    lastName: 'Rheeston'
+};
+
+// Dot notation
+person.name = 'Benjamin';
+
+console.log(person.name);
+
+//Arrays
+let selectedColours = ['red', 'Green', 'blue']; //square brackets indicate empty array
+console.log(selectedColours);
+
+//Functions - Set of statements that performs a task (performing a task) 
+function hello(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Function that calculates a value
+function square(number) {
+    number * number;
+}
+
+square(2);
+
+
+
